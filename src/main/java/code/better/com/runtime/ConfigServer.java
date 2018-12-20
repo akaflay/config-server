c3a2154120ced118813618be94c828791d54a077
Angad <angadkafley@gmail.com>
2018-12-19 23:48:56 -0800

    Syncing project information

--- src/main/java/code/better/com/runtime/ConfigServer.java
+++ src/main/java/code/better/com/runtime/ConfigServer.java
@@ -1,4 +1,4 @@
-package bettercoder.config.config.server;
+package code.better.com.runtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigServer.class, args);
 	}
 }
